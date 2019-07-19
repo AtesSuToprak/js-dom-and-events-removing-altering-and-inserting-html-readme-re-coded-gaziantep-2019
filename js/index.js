5430bd0b7d714d5224af0501d33dc6225e15b178
Hakan Yildiz <hakanyildiz248@gmail.com>
2019-07-19 11:42:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,7 +10,8 @@ let ul = document.createElement('ul');
 
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
-  li.innerHTML = (i + 1).toString();
+  li.innerHTML = (i + 1);
   ul.appendChild(li);
 }
 element.appendChild(ul);
+
